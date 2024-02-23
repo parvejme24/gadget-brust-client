0d4e98c1130c86856ae2a3c63d041878bdf880fc
parvejme24 <mdparvejme24@gmail.com>
2024-02-23 21:24:02 +0600

    added error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <div></div>;
+export default function App() {
+  return <div>App</div>;
 }
-
-export default App;
