c6921dc6dc3b342907e176bede5a1168f5a07f45
parvejme24 <mdparvejme24@gmail.com>
2024-02-26 00:45:54 +0600

    added banner in home page

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+import banner from "../../../assets/banner-bg.png";
+
+const bannerData = [
+  {
+    image:
+      "https://m.media-amazon.com/images/I/71lrSsFkE7L.__AC_SY445_SX342_QL70_FMwebp_.jpg",
+    subtitle: "best headphone in 2024",
+    title: "MOVSSOU E7 Active Noise Cancelling Headphone",
+  },
+  {
+    image:
+      "https://m.media-amazon.com/images/I/71lrSsFkE7L.__AC_SY445_SX342_QL70_FMwebp_.jpg",
+    subtitle: "best headphone in 2024",
+    title: "MOVSSOU E7 Active Noise Cancelling Headphone",
+  },
+  {
+    image:
+      "https://m.media-amazon.com/images/I/71lrSsFkE7L.__AC_SY445_SX342_QL70_FMwebp_.jpg",
+    subtitle: "best headphone in 2024",
+    title: "MOVSSOU E7 Active Noise Cancelling Headphone",
+  },
+  {
+    image:
+      "https://m.media-amazon.com/images/I/71lrSsFkE7L.__AC_SY445_SX342_QL70_FMwebp_.jpg",
+    subtitle: "best headphone in 2024",
+    title: "MOVSSOU E7 Active Noise Cancelling Headphone",
+  },
+  {
+    image:
+      "https://m.media-amazon.com/images/I/71lrSsFkE7L.__AC_SY445_SX342_QL70_FMwebp_.jpg",
+    subtitle: "best headphone in 2024",
+    title: "MOVSSOU E7 Active Noise Cancelling Headphone",
+  },
+];
+
+export default function Banner() {
+  return (
+    <div className="banner-bg">
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {bannerData.map((item) => (
+          <SwiperSlide>
+            <div className="container mx-auto grid grid-cols-2 justify-between items-center py-14 md:py-28 px-3 md:px-0">
+              <div className="space-y-4">
+                <h4 className="uppercase text-lg md:text-xl text-red-600">
+                  {item.subtitle}
+                </h4>
+                <h2 className="text-4xl capitalize font-semibold hidden md:flex">
+                  {item.title}
+                </h2>
+                <button className="bg-[#FF6E06] text-white px-4 py-2 rounded-md">
+                  Buy Now
+                </button>
+              </div>
+              <div className="flex justify-center items-center">
+                <img src={item.image} alt="" className="max-w-[70%]" />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Usual from "../../components/Home/Usual/Usual";
+import Banner from "../../components/Home/Banner/Banner";
 
 export default function Home() {
   return (
     <div>
+      <Banner />
       <Usual />
     </div>
   );
